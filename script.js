969749cfe6aff3e06086cc445dbdabca3c31ef21
MrMa-rio <112214759+MrMa-rio@users.noreply.github.com>
2023-01-19 11:17:58 -0300

    mudando cores com javascript

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-const changeBackColor = document.body;
-const changeDivColor = document.getElementById("login")
+function mixcolors(){
+    const changeBackColor = document.body.style
+    const changeDivColor = document.getElementById("login")
 
-let colorIndex = 0;
-const colors = ["#750706a8", "#02111B","grey"];
-
-setInterval(() => {
-    if(colorIndex == 1){
-        //fazer com que as letras mudem junto com base na sua cor
-        // salvar essa versao no modo customizado 
-    }
-    changeBackColor.style.backgroundImage = `linear-gradient(to bottom, ${colors[colorIndex]}, black)`;
+    let colorIndex = 0
     
-    changeDivColor.style.backgroundColor = colors[colorIndex]
-    colorIndex = (colorIndex + 1) % colors.length;
-}, 1500);
+    
+    const mixOfColors = []
+    const mixOfColors2 = []
+    let numeroIndex2 = 0
+
+    for(let x = 0; x<100;x++){
+
+        
+
+            const color = `rgb(${x}, ${x}, ${x})`
+            const color2 = `rgb(${100-x}, ${100-x}, ${100-x})`
+            mixOfColors.push(color)
+            mixOfColors2.push(color2)
+        
+        
+    }
+    for(let x = 100; x>=1;x--){
+
+        
+
+        const color = `rgb(${x}, ${x}, ${x})`
+        const color2 = `rgb(${numeroIndex2}, ${numeroIndex2}, ${numeroIndex2})`
+        mixOfColors.push(color)
+        mixOfColors2.push(color2)
+        numeroIndex2++
+    
+    
+}
+
+    setInterval(() => {
+        
+        changeBackColor.backgroundImage = `linear-gradient(to bottom, ${mixOfColors[colorIndex]}, ${mixOfColors2[colorIndex]})`
+    
+        
+        colorIndex = (colorIndex + 1) % mixOfColors.length
+    }, 70)
+
+}
+
+mixcolors() 
 
